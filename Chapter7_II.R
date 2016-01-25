71221e716c2d33ee9b23aa70568f95f62660a33c
Kid-Cudi <ormeno.joshua@gmail.com>
2016-01-25 15:54:43 -0800

    stats hw

--- Chapter7_II.R
+++ Chapter7_II.R
@@ -12,6 +12,7 @@ regr <- lm(sales_data$sales.in1000. ~ sales_data$number.of.sales)
 summary(regr) #outputs all of the coefficients, etc.
 plot(sales_data$number.of.sales, sales_data$sales.in1000., #use auto complete here
      xlab="number of people working",
+     
      ylab="sales ($1000)",
      main="Bookstore")
 abline(regr, col="red")
@@ -52,3 +53,30 @@ sprintf("The residule for 18 people is $%.1fk.  This means the model is an under
 #####################
 
 #a Write the regression equation. Define the variables used in your equation. 
+
+# use lm(y ~ x) to find the linear model
+
+Disk_data <- read.csv("Disk_Data.csv")
+
+regr <- lm(Disk_data$Price..in... ~ Disk_data$Capacity..in.TB)
+summary(regr)
+plot(Disk_data$Capacity..in.TB.,Disk_data$Price..in...,  #use auto complete here
+     xlab="amount of capacity that can be held ",
+     ylab="price in dollars ",
+     main="Disk data")
+abline(regr, col="red")
+
+
+#b########
+
+
+# (b) - interpret slope
+# an increase in the capacity that a disk drive can hold increases the sale price,
+# The price goes up by about $104 for every 1 TB 
+# is about $913.40, according to the model
+
+# (c) - interpret y-intercept
+# When zero sales people are working, the bookstore makes about
+# $8100 in sales.  This might have meaning.  Perhaps the bookstore
+# is making sales online as well.
+
