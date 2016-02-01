540565a8f1f4292dc99bcbc1397efd0c1d69eeb0
Kid-Cudi <ormeno.joshua@gmail.com>
2016-02-01 13:41:28 -0800

    finished these

---
+++ chapter 7 III.R
@@ -0,0 +1,20 @@
+ #############
+ ###25
+##############
+ 
+ ##A. The explantory variable x, the response variable y. 
+  ## The explantory variable is size the reponse variable is price
+ ##b## - 1000$ per square feet 
+ ##c##  I think that the slope positive.  
+ 
+ ######
+ #26
+ ###
+ 
+## A.  The explantory variable is Drop, the response variable is duration
+ #B  Secounds per feet 
+ #C I think the slope is positive because a larger drop means a longer duration
+ 
+ ## 31. 
+ 
+ 
