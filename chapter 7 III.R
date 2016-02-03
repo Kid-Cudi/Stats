288f4195086ae1b1d7e599a4b44e9b4bd09a07e2
Kid-Cudi <ormeno.joshua@gmail.com>
2016-02-03 09:33:30 -0800

    3 more

--- chapter 7 III.R
+++ chapter 7 III.R
@@ -16,5 +16,30 @@
  #C I think the slope is positive because a larger drop means a longer duration
  
  ## 31. 
+ #A. Price increases by 0.061 per additional sq ft
+ #B 230.82 thousand dollars
+ #c I would expect to pay 121002 thousand dollars on this house
+ # the asking price of the house if 115002 thousand dollars 
+ # the 6000 is called the residule 
+ price<-function(size){
+  return(47.82+.061*size) 
+ }
+ estimate<-price(3000)
+ #c
+ price<-function(size){
+   return(47.82+0.061*size)
+   
+ }
+   estimate<-price(1200)
  
+ #32 
+ #A For every one foot the duration of the ride will increase by .180 secounds
+ #B
+ 
+ 
+duration <- function(Drop){
+  return(64.232+0.180*Drop)
+}
+
+estimate <- duration(200)
  
